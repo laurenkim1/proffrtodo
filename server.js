f18909141ff6c4abd402d0e7ba022bec98d00165
laurenkim1 <laurenkim@college.harvard.edu>
2017-10-31 16:36:28 -0400

    jk

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 var app = require('./app');
-var port = process.env.PORT || 3000 || 443 || 80;
+var port = process.env.PORT || 3000;
 
 var server = app.listen(port, function() {
   console.log('todo list RESTful API server started on: ' + port);
