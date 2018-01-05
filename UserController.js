b2e91c0580681ff8c60472ec9b30ddf634ea874a
laurenkim1 <laurenkim@college.harvard.edu>
2018-01-05 01:03:10 -0800

    test

--- UserController.js
+++ UserController.js
@@ -36,7 +36,7 @@ router.get('/', function (req, res) {
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:id', function (req, res) {
-    db.collection("users").find({ userId: req.params.id }, function (err, user) {
+    User.find({ userId: req.params.id }, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         res.status(200).send(user);
